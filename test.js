b429f9cefb148d344746fe95366379d16b527dc1
Unknown <hamzaed-JR@outlook.com>
2019-03-25 11:35:37 +0100

    final

---
+++ test.js
@@ -0,0 +1,14 @@
+function toggleNavPanel(x){
+  var panel = document.getElementById(x), maxH="110px";
+  if(panel.style.height == maxH){
+      panel.style.height = "0px";
+  } 
+    else {
+      panel.style.height = maxH;
+      }
+  }
+
+
+
+
+ 
